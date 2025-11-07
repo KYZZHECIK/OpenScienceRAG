bbc2e034b89b8b69d6e335e0eb75324e142ea4e2
Ahmadov Avazagha <ahmadov.avazagha@gmail.com>
2025-11-07 15:33:25 +0100

    Added argument parameters

--- src/open_science_rag/open_alex_calls.py
+++ src/open_science_rag/open_alex_calls.py
@@ -1,10 +1,25 @@
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from langchain_openai import ChatOpenAI
 import src.open_science_rag.query_filtering.search_service as QFiltering
+from src.open_science_rag.utils import get_secrets
+
+parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+parser.add_argument('--user_question', type=str,
+                    default="What is RAG?", help="User's question that will be used to gather context and answered")
+parser.add_argument('--model_name', type=str,
+                    default="gpt-4o-mini", help="Model name from OpenAI API")
+parser.add_argument('--model_temperature', type=float,
+                    default=0.0, help="Temperature for OpenAI model")
 
 
 if __name__ == '__main__':
+    args = parser.parse_args()
+    get_secrets()
+    llm = ChatOpenAI(model=args.model_name, temperature=args.model_temperature)
+
     service = QFiltering.SearchService(
-        translator=QFiltering.QueryTranslator(),
+        translator=QFiltering.QueryTranslator(llm=llm),
         entity_client=QFiltering.EntityClient()
     )
 
-    results = service.search("Who asked?")
+    results = service.search(args.user_question)
